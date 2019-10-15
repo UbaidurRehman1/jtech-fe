b05985a25f676cc60c59680a21b7b664d32b5b1c
UbaidurRehman1 <ubaidshan007@gmail.com>
2019-10-15 16:03:44 +0500

    views generate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,12 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'chatbox', loadChildren: './views/chatbox/chatbox.module#ChatboxPageModule' },
+  { path: 'sessions', loadChildren: './views/chatbox/sessions/sessions.module#SessionsPageModule' },
+  { path: 'conversation', loadChildren: './views/chatbox/sessions/conversation/conversation.module#ConversationPageModule' },
+  { path: 'discover', loadChildren: './views/chatbox/discover/discover.module#DiscoverPageModule' },
+  { path: 'detail', loadChildren: './views/chatbox/detail/detail.module#DetailPageModule' },
+  { path: 'auth', loadChildren: './views/auth/auth.module#AuthPageModule' },
 ];
 
 @NgModule({
